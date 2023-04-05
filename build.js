8413909afaa02907c6110bbc16c1484325e8a18b
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-04-05 11:13:03 +0200

    Updates for building

---
+++ build.js
@@ -0,0 +1,17 @@
+import { build } from '@sanjo/add-on-builder'
+import { readdir } from 'fs/promises'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+import * as process from 'node:process'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const addOnsPath = join(__dirname, 'AddOns')
+const addOnNames = (await readdir(addOnsPath, {withFileTypes: true}))
+  .filter(entry => entry.isDirectory())
+  .map(entry => entry.name)
+
+for (const addOnName of addOnNames) {
+  process.chdir(join(addOnsPath, addOnName))
+  await build()
+}
